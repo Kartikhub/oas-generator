a769b340944f54eaf12e07b159569a1cf14089b5
Kartikhub <sharmakartik66@gmail.com>
2024-03-07 00:01:56 +0530

    The prompt is improved, provided roles to different kind of messages.

--- src/main/java/com/crent/service/AIService.java
+++ src/main/java/com/crent/service/AIService.java
@@ -2,33 +2,39 @@ package com.crent.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Slf4j
 @Service
 public class AIService {
-
-    private static final String INITIAL_PROMPT = "IGNORE all the previous PROMPTS and start FRESH. Create an OpenAPI Spec using the below-provided DETAILS and STRICTLY follow the RULES for creating the openapi spec-\n" +
-            "RULES\n" +
-            "1. ONLY PROVIDE THE OpenAPI Spec. Remember do not write anything else. I ONLY need a Yaml code snippet that's it. Also take time but never give just a stub. \n" +
-//            "2. If in DETAILS section, SCHEMA and PATH information is not given, then always create a corresponding schema(only name and id) and one corresponding path with get HTTP method. \n" +
-//            "\n" +
-            "2. Always provide Paths and schemas, and do not provide response or request for the Paths unless it is mentioned. And if Request and response body are asked to be created then provide the reference to the models in components" +
-            "EXCEPTION OF RULES-\n" +
-            "If in the DETAILS section, the information provided is not about how to create a OpenAPI Spec or there is no detail at all then skip all the Rules and provide the result as ERROR-Not a valid prompt for OpenAPI Spec. - ( THIS WILL BE THE ONLY EXCEPTION OF RULE 1)\n" +
-            "\n" +
-            "DETAILS-\n";
-
-    private static final String DEFAULT_PROMPT= "Create an OpenAPI Spec which has below details\n" +
-            "1. Schemas\n" +
-            "  a. Car having properties like engine string type, speed integer type, id integer type\n" +
-            "2. Paths\n" +
-            "a. Post method to add car \n" +
-            "b. get method to get the car details using the car id\n" +
-            "\n" +
-            "ONLY PROVIDE THE OpenAPI Spec without any other detail. Remember do not write anything else. I ONLY need a String of it that's it";
+    private static final SystemMessage SYSTEM_MESSAGE = new SystemMessage(
+            "1. You are required to create an OpenAPI Spec and nothing else.\n" +
+                    "2. Your output should strictly adhere to the provided rules.\n" +
+                    "3. Provide only a YAML code snippet of the OpenAPI Spec containing Paths, schemas, and components.\n" +
+                    "4. Do not include response or request bodies for the Paths unless specified; in such cases, provide references to the models in components.\n" +
+                    "5. If the provided details do not pertain to creating an OpenAPI Spec, or if there's no detail at all, skip all rules and output \"ERROR-Not a valid prompt for OpenAPI Spec.\"\n" +
+                    "6. Clearly specify the required OpenAPI version (e.g., OpenAPI 3.x).\n" +
+                    "7. Include information about the base path and any API servers if applicable.\n" +
+                    "8. Clearly define the HTTP methods (GET, POST, PUT, DELETE, etc.) for each path.\n" +
+                    "9. Specify the expected data types and formats for request and response payloads.\n" +
+                    "10. Clearly mention any authentication mechanisms or security requirements.\n" +
+                    "11. If applicable, include details about query parameters, path parameters, and request headers.\n" +
+                    "12. Clearly define the status codes and their meanings for each path.\n" +
+                    "13. Use appropriate OpenAPI conventions for naming and structuring components.\n" +
+                    "14. Ensure consistency in the naming of paths, parameters, description, summary and schemas.\n" +
+                    "15. Handle edge cases or error scenarios if specified in the DETAILS section.\n" +
+                    "16. If there are specific rules or conventions to follow in the DETAILS section, incorporate them in the OpenAPI Spec.\n" +
+                    "\n" +
+                    "DETAILS-\n"
+    );
 
     private static final String OAS_PREFIX = "openapi";
 
@@ -41,13 +47,13 @@ public class AIService {
 
 
     public String getOpenAPISpec(String message) {
-        String promptMessage = INITIAL_PROMPT + message;
-        String callResult = chatClient.call(promptMessage);
-        log.info("Result- {}", callResult);
-        //Make it fast by removing the char using index
-        String result = callResult.replace("```yaml\n", "").replace("\n```", "").trim();
+        Prompt prompt = new Prompt(List.of(SYSTEM_MESSAGE, new UserMessage(message)));
+        ChatResponse chatResponse = chatClient.call(prompt);
+        String rawMessage = chatResponse.getResults().get(0).getOutput().getContent();
+        log.info("Result {}: ", rawMessage);
+        String result = rawMessage.replace("```yaml\n", "").replace("\n```", "").trim();
         if(!result.startsWith(OAS_PREFIX)) {
-            log.info("illegal argument exception");
+            log.info("Illegal Argument Exception - Provided prompt was not correct.");
             throw new IllegalArgumentException("Please provide a appropriate Prompt");
         }
         return result;
